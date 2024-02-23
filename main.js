0800403af146b999e7dac6307b9827c20acb643c
dev <adriandevilla0518@gmail.com>
2024-02-23 17:49:28 +0800

    Edit the about me part and remove skills content

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".typing-animation", {
+    strings:["SOFTWARE DEVELOPER", "WEBSITE DEVELOPER"],
+    typeSpeed: 50,
+    backSpeed: 10,
+    backDelay: 1000,
+    loop: true,
+});
