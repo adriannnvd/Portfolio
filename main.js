9eb05f67e4ca0733a07a489ecd2f3bd231df8f2e
dev <adriandevilla0518@gmail.com>
2024-02-26 13:41:35 +0800

    fix the animation in home page and hover in experiences paga

--- main.js
+++ main.js
@@ -1,29 +1,58 @@
+//CHANGE COLOR TO LOGO
 const letters = document.querySelectorAll('.logo .letter');
 
 function toggleColorChange() {
     letters.forEach((letter, index) => {
         setTimeout(() => {
             letter.classList.toggle('color-change');
-        }, index * 100); // Adjust the delay as needed
+        }, index * 100);
     });
 }
 
 setInterval(toggleColorChange, 2000);
 
-
+//CHANGE COLORS IN NAV-BUTTON
 document.addEventListener("DOMContentLoaded", function() {
     const navButtons = document.querySelectorAll('.nav-button');
   
     navButtons.forEach(function(button) {
       button.addEventListener('click', function() {
-        // Remove 'active' class from all buttons
+        
         navButtons.forEach(function(btn) {
           btn.classList.remove('active');
         });
   
-        // Add 'active' class to the clicked button
+        
         this.classList.add('active');
       });
     });
   });
   
+
+//TYPING ANIMATION TO SOTWARE DEVELOPER
+document.addEventListener('DOMContentLoaded', function () {
+            var elements = document.querySelectorAll('.typing');
+            elements.forEach(function (element) {
+                var text = element.textContent;
+                var intervalId;
+
+                function startTyping() {
+                    element.textContent = '';
+                    var index = 0;
+                    intervalId = setInterval(function () {
+                        if (index < text.length) {
+                            var letterSpan = document.createElement('span');
+                            letterSpan.textContent = text[index];
+                            element.appendChild(letterSpan);
+                            index++;
+                        } else {
+                            clearInterval(intervalId);
+                            setTimeout(startTyping, 1000); 
+                        }
+                    }, 100); 
+                }
+
+                startTyping();
+            });
+        });
+
